a7fb664b6485bf06f4dd8c74a4c2b1eaa875aac0
mouad Talhaoui <talhaouimouad04@gmail.com>
2024-08-09 17:56:42 +0100

    adding scroll to first window

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import requests
 from tkinter import *
 from tkinter import ttk
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from tkhtmlview import HTMLLabel
+
+root = Tk()
+root.title("Find Job")
+#root.iconbitmap("path")
+root.geometry("500x500")
+
+#Create a main frame
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH, expand=1)
+#create a canvas
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+# add a scrollbar to the canvas
+my_scrollbary = ttk.Scrollbar(main_frame,orient=VERTICAL, command=my_canvas.yview)
+
+my_scrollbarx = ttk.Scrollbar(main_frame,orient=HORIZONTAL, command=my_canvas.xview)
+my_scrollbarx.pack(side=BOTTOM,fill=X)
+my_scrollbary.pack(side=RIGHT,fill=Y)
+#configure the canvas
+my_canvas.configure(xscrollcommand=my_scrollbarx.set, yscrollcommand=my_scrollbary.set)
+my_canvas.bind('<Configure>',lambda e:my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+
+#create anthoer frame inside the canvas
+second_frame = Frame(my_canvas)
+
+
+#add that new frame to a window in the canvas
+my_canvas.create_window((0,0),window=second_frame,anchor="nw")
 
 
 def generatefile(m):
@@ -28,10 +57,10 @@ def generatefile(m):
                 if len(data)!=0:
                     title1 = data[0].get_text()
                     link = data[0]['href']
-                    concours.append([title1,link])
+                    concours.append([title1,"https://www.emploi-public.ma/fr/"+link])
 
     # Store the information in a pandas dataframe
-    df = pd.DataFrame(concours, columns=['Title',"link"])
+    df = pd.DataFrame(concours, columns=['Title',"link for more details"])
 
 
     # Export the data to a CSV file
@@ -43,7 +72,7 @@ value = []
 def searchAndGeneratefile(m):
     global value 
     value = generatefile(m)
-    visaluserConcours(frm, value)
+    visaluserConcours(second_frame, value)
     
 
 def filterData(word):
@@ -53,26 +82,21 @@ def filterData(word):
     value = list(filter(lambda x: word.lower() in x[0].lower(), value))
     print("Ffff")
     print(value)
-    visaluserConcours(frm, value)
-
-root = Tk()
-root.title("Find public job | Mouad")
+    visaluserConcours(second_frame, value)
 
 
-global frm 
-frm= ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="how mauch pages you want").grid(column=0, row=0)
-e1 = ttk.Entry(frm)
+ttk.Label(second_frame, text="how mauch pages you want").grid(column=0, row=0)
+e1 = ttk.Entry(second_frame)
 e1.grid(column=1, row=0)
-e2 = ttk.Entry(frm)
-ttk.Label(frm, text="Do you want to filter data").grid(column=0, row=1)
+e2 = ttk.Entry(second_frame)
+ttk.Label(second_frame, text="Do you want to filter data").grid(column=0, row=1)
 e2.grid(row=1, column=1)
-ttk.Button(frm, text="Search and generate file", command=lambda:searchAndGeneratefile(e1.get())).grid(column=1, row=2)
-ttk.Button(frm, text="filter data", command=lambda:filterData(e2.get())).grid(column=0, row=2)
+ttk.Button(second_frame, text="Search and generate file", command=lambda:searchAndGeneratefile(e1.get())).grid(column=1, row=2)
+ttk.Button(second_frame, text="filter data", command=lambda:filterData(e2.get())).grid(column=0, row=2)
 labels = []
 btns = []
-def visaluserConcours(frm, value):
+
+def visaluserConcours(second_frame, value):
     global labels
     global btns
     for lb in labels:
@@ -83,22 +107,19 @@ def visaluserConcours(frm, value):
             btn.destroy()
     if len(value)!=0:
         for i in range(len(value)):
-            label = ttk.Label(frm, text=value[i][0])
+            label = ttk.Label(second_frame, text=value[i][0])
             labels.append(label)
             label.grid(column=1, row=3+i)
             
         for i in range(len(value)):
-            button = ttk.Button(frm, text="voir details", command=lambda:moreDetails(value[i][1]))
+            button = ttk.Button(second_frame, text="voir details", command=lambda:moreDetails(value[i][1]))
             btns.append(button)
             button.grid(column=0, row=3+i)
 
-visaluserConcours(frm, value)
+visaluserConcours(second_frame, value)
 
 def moreDetails(link):
-    print(link)
-    url = "https://www.emploi-public.ma/fr/"
-    response = requests.get(url+link)
-
+    response = requests.get(link)
     # Parse the HTML code using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
     data = []
@@ -110,7 +131,7 @@ def moreDetails(link):
     print("ths")
     print(ths)
     openNewWindow(data,tds,ths)
-    
+
 
 def openNewWindow(data,tds,ths):
      
@@ -136,5 +157,4 @@ def openNewWindow(data,tds,ths):
     # Adjust label
     my_label.pack()
  
-    
 root.mainloop()
